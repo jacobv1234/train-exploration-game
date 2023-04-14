ae21d515399c1fcda370366f6343cabfb5309b4a
jacobv123 <94700520+jacobv1234@users.noreply.github.com>
2023-04-14 21:12:53 +0100

    Add files via upload

--- testing/driving/main.py
+++ testing/driving/main.py
@@ -1,6 +1,14 @@
 from tkinter import *
+from time import sleep
+
 window = Tk()
-c = Canvas(window, width = 350, height = 250, bg = 'white')
+
+screen_width = window.winfo_screenwidth() - 10
+screen_height = window.winfo_screenheight() - 75
+
+window.state('zoomed')
+
+c = Canvas(window, width = screen_width, height = screen_height, bg = 'white')
 c.pack()
 
 directions = ( #(x,y)
@@ -28,5 +36,43 @@ class Train():
         self.x = startx
         self.y = starty
         self.direction = startdir
+        self.speed = 0
         self.object = c.create_rectangle(startx-10, starty-10, startx+10, starty+10, outline='black')
+        c.bind_all('<Left>', self.turn_left)
+        c.bind_all('<Right>', self.turn_right)
+        c.bind_all('<Up>', self.speed_up)
+        c.bind_all('<Down>', self.slow_down)
 
+    def turn_left(self, event):
+        self.direction -= 1
+        self.direction %= 16
+    
+    def turn_right(self, event):
+        self.direction += 1
+        self.direction %= 16
+    
+    def speed_up(self, event):
+        if self.speed == 0:
+            self.speed = 4
+        elif self.speed == 4:
+            self.speed = 8
+    
+    def slow_down(self, event):
+        if self.speed == 8:
+            self.speed = 4
+        elif self.speed == 4:
+            self.speed = 0
+
+    def move_train(self, c = Canvas()):
+        dir_vect = directions[self.direction]
+        c.move(self.object, dir_vect[0] * self.speed, dir_vect[1] * self.speed)
+        
+
+
+
+train = Train(screen_width / 2, screen_height / 2, 0, c)
+
+while True:
+    train.move_train(c)
+    window.update()
+    sleep(0.08)
