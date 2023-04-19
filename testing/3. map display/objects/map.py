7aeee55c44187edbf3cbf822df0a7c38162462a0
jacobv123 <94700520+jacobv1234@users.noreply.github.com>
2023-04-19 09:45:47 +0100

    Add files via upload

---
+++ testing/3. map display/objects/map.py
@@ -0,0 +1,13 @@
+from json import loads
+from objects.line import Line
+
+class Map():
+    def __init__(self, name, canvas):
+        # load the manifest file
+        with open(f'/{name}/manifest.json', 'r') as f:
+            manifest = loads(f.read().strip('\n'))
+        
+        # load each line
+        self.lines = []
+        for line in manifest['lines']:
+            self.lines.append(Line(name, line, canvas))
