a3766c4ba36c21e9a74dc402062c6b677e53a00e
jacobv123 <94700520+jacobv1234@users.noreply.github.com>
2023-05-02 07:58:20 +0100

    Add files via upload

---
+++ testing/13. spritesheet/main.py
@@ -0,0 +1,79 @@
+from tkinter import *
+from time import sleep
+
+# import objects
+from lib.map import Map
+from lib.train import Train
+from lib.speedtracker import SpeedTracker
+from lib.junction_choice import JunctionChoice
+
+window = Tk()
+
+screen_width = window.winfo_screenwidth() - 10
+screen_height = window.winfo_screenheight() - 75
+
+window.state('zoomed')
+
+c = Canvas(window, width = screen_width, height = screen_height, bg = 'white', xscrollincrement=1, yscrollincrement=1)
+c.place(x=3,y=0)
+
+# create the map
+start_map = 'map'
+# called area cause map is a function
+area = Map(start_map, c)
+
+# hud
+speedtracker = SpeedTracker(window,screen_width,screen_height)
+
+# train
+train = Train(area.start[0], area.start[1], area.start[2], area.start_line, c, skin='default')
+
+# center on screen
+c.xview_scroll(area.start[0] - (round(screen_width / 2)), 'units')
+c.yview_scroll(area.start[1] - (round(screen_height / 2)), 'units')
+
+# initialise certain variables
+junc = False
+
+# loop
+while True:
+    train.move_train(c)
+
+    corner = area.check_corners(train.x, train.y, train.line)
+    if corner != 0:
+        train.corner(corner)
+
+    if junc != False:
+        if junc['coords'][0] == train.x and junc['coords'][1] == train.y:
+            train.junction(junc, chooser.options[chooser.choice])
+            junc = False
+            chooser.close()
+            del chooser
+    
+    stop = area.check_stops(train.x,train.y,train.line)
+    if stop != 0:
+        train.stop(stop, c)
+
+    junction = area.check_j_approach(train.x,train.y,train.direction, train.line)
+    if junction != 0:
+        chooser = JunctionChoice(junction,window,screen_width)
+        junc = junction
+    
+    # update HUD
+    match train.speed:
+        case 0:
+            destination = 280
+        case 1:
+            destination = 200
+        case 2:
+            destination = 120
+        case 4:
+            destination = 40
+    speedtracker.update(destination)
+
+    try:
+        chooser.update()
+    except:pass
+
+    window.update()
+    sleep(0.015)
