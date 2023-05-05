3407b5cebf72ffd9e8138cf348a941ca108ee9b8
jacobv123 <94700520+jacobv1234@users.noreply.github.com>
2023-05-05 15:35:02 +0100

    Completed version 16
    This version adds the ability for stations to have text associated with them, allowing for names, and adjusts the graphic that represents stations so it feels less awkward driving over them.

---
+++ testing/16. station names/lib/line.py
@@ -0,0 +1,46 @@
+from json import loads
+from lib.station import Station
+
+class Line():
+    def __init__(self, map_name, line_name, canvas):
+        self.name = line_name
+        
+        with open(f'./{map_name}/lines/{line_name}.json', 'r') as f:
+            line_data = loads(f.read().strip('\n'))
+
+        self.segments = []
+        for segment in line_data['segments']:
+            self.segments.append(canvas.create_line(segment[0], segment[1], segment[2], segment[3], fill=line_data['colour']))
+        
+        self.corners = line_data['corners']
+
+        self.stops = line_data['stops']
+
+        self.junctions = line_data['junctions']
+
+        self.stations = [Station(name, canvas) for name in line_data['stations']]
+    
+    def check_corners(self, x,y):
+        for corner in self.corners:
+            if corner[0] == x and corner[1] == y:
+                return corner
+        return 0
+
+    def check_stops(self, x,y):
+        for stop in self.stops:
+            if stop[0] == x and stop[1] == y:
+                return stop
+        return 0
+
+    def check_j_approach(self, x,y,dir):
+        for junction in self.junctions:
+            if junction['approach']['coords'][0] == x and junction['approach']['coords'][1] == y and dir == junction['approach']['direction']:
+                return junction
+        return 0
+
+    def check_stations(self, x, y):
+        for station in self.stations:
+            pos = station.pos
+            if x in range(pos[0]-12, pos[0]+12) and y in range(pos[1]-12, pos[1]+12):
+                return station
+        return 0
---
+++ testing/16. station names/lib/map.py
@@ -0,0 +1,29 @@
+from json import loads
+from lib.line import Line
+
+class Map():
+    def __init__(self, name, canvas):
+        # load the manifest file
+        with open(f'./{name}/manifest.json', 'r') as f:
+            manifest = loads(f.read().strip('\n'))
+        
+        # load each line
+        self.lines = {}
+        for line in manifest['lines']:
+            line_object = Line(name, line, canvas)
+            self.lines[line_object.name] = line_object
+        
+        self.start = manifest['start']
+        self.start_line = manifest['start_line']
+    
+    def check_corners(self, x, y, line):
+        return self.lines[line].check_corners(x,y)
+    
+    def check_stops(self, x, y, line):
+        return self.lines[line].check_stops(x,y)
+
+    def check_j_approach(self, x, y, dir, line):
+        return self.lines[line].check_j_approach(x,y,dir)
+
+    def check_stations(self,x,y,line):
+        return self.lines[line].check_stations(x,y)
